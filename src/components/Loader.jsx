2cc40b7627d9be85df506eaef5ba6e060c5d871b
Benson Tran <benson.trn@gmail.com>
2020-09-28 10:50:38 -0400

    Fetch packages from firebase, added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Loader.css';
+
+function Loader() {
+
+  return (
+    <div>
+      <div className="loader"></div>
+    </div>
+
+  )
+}
+
+export default Loader;
--- src/components/PackageList.jsx
+++ src/components/PackageList.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase/firebase';
+import Loader from './Loader';
 
 function PackageList() {
+  const [loading, setLoading] = useState(true);
+  const [packages, setPackages] = useState();
+
+  useEffect(() => {
+    const unsub = db.collection('packages').onSnapshot(snap => {
+      const data = snap.docs.map(doc => doc.data())
+      console.log(data);
+      setPackages(data);
+      setLoading(false);
+    });
+
+    return () => unsub();
+  }, []);
+
   return (
+    loading ? <Loader /> :
     <div>
       list
     </div>

f375bea95be62028701eee1d883ea391681118ad
Benson Tran <benson.trn@gmail.com>
2020-10-05 15:46:52 -0400

    Updates

---
+++ precache-manifest.e2a264e2998d05045cd843b9bd05c49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3f4e81f89682aed46552715099546c",
+    "url": "/package-tracker/index.html"
+  },
+  {
+    "revision": "76008a299f343daf8349",
+    "url": "/package-tracker/static/css/main.a36062bb.chunk.css"
+  },
+  {
+    "revision": "1836fcd1e5c3f944f7f2",
+    "url": "/package-tracker/static/js/2.a9e4e7e9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/package-tracker/static/js/2.a9e4e7e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76008a299f343daf8349",
+    "url": "/package-tracker/static/js/main.070039e3.chunk.js"
+  },
+  {
+    "revision": "ae24255c3e73d632160d",
+    "url": "/package-tracker/static/js/runtime-main.f12ed79f.js"
+  }
+]);
